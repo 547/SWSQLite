4f53a1b26702882485d17f9fef3b42974943a978
Seven Wang <seven.wang@supernovachina.com>
2019-06-20 14:41:48 +0800

    Initial Commit

---
+++ SWSQLite/SWSQLite.h
@@ -0,0 +1,19 @@
+//
+//  SWSQLite.h
+//  SWSQLite
+//
+//  Created by Supernova SanDick SSD on 2019/6/20.
+//  Copyright © 2019 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWSQLite.
+FOUNDATION_EXPORT double SWSQLiteVersionNumber;
+
+//! Project version string for SWSQLite.
+FOUNDATION_EXPORT const unsigned char SWSQLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWSQLite/PublicHeader.h>
+
+
